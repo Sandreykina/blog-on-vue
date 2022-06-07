1b4b46e26da708b170081ccfd61549aecdc2f2bb
DESKTOP-FHT2P65\Elena <hellens.rez15@gmail.com>
2022-06-08 02:25:53 +0300

    add csrf token

--- src/axios-api.js
+++ src/axios-api.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
 const getApi = axios.create({
+    xsrfCookieName: 'csrftoken',
+    xsrfHeaderName: "X-CSRFTOKEN",
+    headers: {
+        'X-CSRFToken': 'csrftoken',
+    },
     baseURL: 'http://127.0.0.1:8000',
 })
 
